e35db6292d6ae0a56f14fb91739999feb16287c3
Developer <68657182+kartvya@users.noreply.github.com>
2024-08-20 12:03:48 +0530

    basic setup done

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Colors } from "@/constants/Colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import { StyleSheet, useColorScheme } from "react-native";
+
+export default function TabLayout() {
+  const theme = useColorScheme() ?? "light";
+  const activeColor = theme === "light" ? Colors.dark.primeColor : "#EF9337";
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: activeColor,
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={({ navigation, route }) => ({
+          tabBarIcon: ({ color, focused }) => (
+            <FontAwesome
+              size={28}
+              name="home"
+              color={focused ? activeColor : color}
+            />
+          ),
+        })}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={({ navigation, route }) => ({
+          tabBarIcon: ({ color, focused }) => (
+            <FontAwesome
+              size={28}
+              name="cog"
+              color={focused ? activeColor : color}
+            />
+          ),
+        })}
+      />
+    </Tabs>
+  );
+}
+
+const styles = StyleSheet.create({});
