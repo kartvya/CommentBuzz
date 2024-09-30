78d989e5911251544a89f22e164a9375d1bbc87c
Devloper <68657182+kartvya@users.noreply.github.com>
2024-09-30 19:39:21 +0530

    typescript added

--- src/components/MemoizedCommentView.tsx
+++ src/components/MemoizedCommentView.tsx
@@ -4,7 +4,7 @@ import { Pressable, StyleSheet, TouchableOpacity, View } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { useSelector } from "react-redux";
 import SvgIcon from "../assets/icons";
-import { Colors, DarkColors, useThemeColors } from "../constants/Colors";
+import { Colors, useThemeColors } from "../constants/Colors";
 import { hp } from "../helpers/comman";
 import { RootState } from "../redux/Store";
 import { Users } from "../redux/reducers/AuthReducer";
